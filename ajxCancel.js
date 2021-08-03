e07004192a9a881026d0ee8b2f5bd99815c30e00
EmmanuelleLA <88400903+EmmanuelleLA@users.noreply.github.com>
2021-08-03 20:11:46 -0300

    Delete ajxCancel.js

--- ajxCancel.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    /*==============================================================================
-        jQuery events
-    ==============================================================================*/
-    // Event to go back if clicked in submit cancel
-    jQuery("body").on("click", ".negate", function(){
-        document.location.href="index.php";
-    });
-});
